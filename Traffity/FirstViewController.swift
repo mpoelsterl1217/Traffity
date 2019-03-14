ce2c2bb945458fa987708456f4a95526e5579db1
Matthew Poelsterl <mpoelsterl@i46221p64128304.local>
2019-03-14 14:36:56 -0500

    Initial Commit

---
+++ Traffity/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Traffity
+//
+//  Created by Matthew Poelsterl on 3/14/19.
+//  Copyright © 2019 Matthew Poelsterl. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
